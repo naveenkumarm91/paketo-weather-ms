d374669ac44ad618819ee80c4ecbbe5c1c90ada3
naveenkumarm91 <naveenkumarm0891@gmail.com>
2023-05-10 20:14:11 +0530

    init commit

---
+++ src/main/java/com/app/paketo/PaketoWeatherMsApplication.java
@@ -0,0 +1,13 @@
+package com.app.paketo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaketoWeatherMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaketoWeatherMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/paketo/PaketoWeatherMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.paketo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaketoWeatherMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
